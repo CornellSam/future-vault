744099c1e58620b3f61606201c1eda4af1c5b050
CashJasper <lacmtnxx753726@outlook.com>
2025-11-11 09:15:00 +0800

    feat: add FHE permissions for capsule decryption

--- contracts/TimeCapsule.sol
+++ contracts/TimeCapsule.sol
@@ -1,1 +1,7 @@
-        require(unlockTimestamp > block.timestamp, "Unlock date must be in the future");
+        FHE.allowThis(encryptedPart1);
+        FHE.allowThis(encryptedPart2);
+        FHE.allow(encryptedPart1, decryptManager);
+        FHE.allow(encryptedPart2, decryptManager);
+        FHE.allow(encryptedPart1, msg.sender);
+        FHE.allow(encryptedPart2, msg.sender);
+        emit CapsuleCreated(capsuleId, msg.sender, unlockTimestamp);
