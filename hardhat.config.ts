025d5fd6de99248daf239d11c5c68cbbcd5e26dd
CashJasper <lacmtnxx753726@outlook.com>
2025-11-11 13:30:00 +0800

    feat: configure FHE network settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,18 +1,1 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@fhevm-hardhat-plugin";
-
-const config: HardhatUserConfig = {
-  solidity: "0.8.24",
-  networks: {
-    hardhat: {
-      import "@fhevm-hardhat-plugin";
-    },
-    sepolia: {
-      url: process.env.SEPOLIA_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
-};
-
-export default config;
+      allowUnlimitedContractSize: true,
