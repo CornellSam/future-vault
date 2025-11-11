2421bc922efd7acb759587c1c67ef24dc3c89c2c
CornellSam <oeemrqvh209331@outlook.com>
2025-11-11 10:00:00 +0800

    fix: add existence check in canUnlock

--- contracts/TimeCapsule.sol
+++ contracts/TimeCapsule.sol
@@ -1,7 +1,1 @@
-        FHE.allowThis(encryptedPart1);
-        FHE.allowThis(encryptedPart2);
-        FHE.allow(encryptedPart1, decryptManager);
-        FHE.allow(encryptedPart2, decryptManager);
-        FHE.allow(encryptedPart1, msg.sender);
-        FHE.allow(encryptedPart2, msg.sender);
-        emit CapsuleCreated(capsuleId, msg.sender, unlockTimestamp);
+        require(capsule.exists, "Capsule does not exist");
