c0d848dc16bc699c9ee8c1f31b93dacd301a3196
CashJasper <lacmtnxx753726@outlook.com>
2025-11-16 12:30:00 +0800

    feat: create responsive capsule list component

---
+++ ui/src/components/CapsuleList.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export const CapsuleList = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {/* Capsule cards */}
+    </div>
+  );
+};
