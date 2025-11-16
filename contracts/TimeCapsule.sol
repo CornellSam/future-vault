7d28fb6a636a10ef8197fef9e508de2448deaf7d
CashJasper <lacmtnxx753726@outlook.com>
2025-11-16 13:30:00 +0800

    refactor: optimize storage layout for gas efficiency

--- contracts/TimeCapsule.sol
+++ contracts/TimeCapsule.sol
@@ -1,2 +1,8 @@
-require(unlockTimestamp > block.timestamp + 1 hours, "Unlock time must be at least 1 hour in future");
-        require(encMessagePart1.length > 0, "Message part 1 cannot be empty");
+// Optimized struct packing for gas savings
+    struct Capsule {
+        uint256 unlockTimestamp;
+        address creator;
+        bool exists;
+        euint32 encryptedMessagePart1;
+        euint32 encryptedMessagePart2;
+    }
